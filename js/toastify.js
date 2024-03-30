02adf93ccd96e07ba80a69dab5015c487412e0cc
MoyroxDev <156985060+MoyroxDev@users.noreply.github.com>
2024-03-30 09:47:03 -0400

    Update toastify.js

--- js/toastify.js
+++ js/toastify.js
@@ -1,22 +1,37 @@
-export function mostrarToastify(titulo) {
-    Toastify({
-        text: titulo,
-        duration: 3000,
-        newWindow: true,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "linear-gradient(to right, #2d4997, #4e71cf)",
-          borderRadius: "2rem",
-          fontSize: ".7rem",
-          textTransform: "uppercase"
-        },  
-        offset: {
-            x: "2.5rem", // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-            y: "1.5rem" // vertical axis - can be a number or a string indicating unity. eg: '2em'
-        },
-        onClick: function(){} // Callback after click
-      }).showToast();
+export function mostrarToastify(titulo,size) {
+  let x,y;
+  let key = false;
+
+  if (size > 768) {
+    x = "2.5rem";
+    y = "1.5rem"
+    key = true;
+
+  }else{
+    x = "0rem";
+    y = "0rem";
+  }
+
+  Toastify({
+      text: titulo,
+      duration: 1000,
+      newWindow: true,
+      close: key,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        width: "max-content",
+        background: "linear-gradient(to right, #2d4997, #4e71cf)",
+        borderRadius: "2rem",
+        fontSize: ".7rem",
+        textTransform: "uppercase"
+      },  
+      offset: {
+          x: x, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+          y: y // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+      onClick: function(){} // Callback after click
+  }).showToast();
 }
+
